0fc919b52aef3135e355fc0b56d992d77e243188
krooner <krooner.kim@gmail.com>
2024-04-15 21:45:47 +0900

    Update problemsolving

--- problemsolving/baekjoon/basic/2579/answer.py
+++ problemsolving/baekjoon/basic/2579/answer.py
@@ -1,3 +1,10 @@
 N = int(input())
 points = [int(input()) for _ in range(N)]
 
+scores = [[0, 0] for _ in range(N+1)]
+for i in range(len(points)):
+    idx = i+1
+    scores[idx][0] = max(scores[max(0, idx-2)]) + points[i]
+    scores[idx][1] = scores[max(0, idx-1)][0] + points[i]
+
+print(max(scores[N]))
